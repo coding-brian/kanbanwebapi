d5b053a5298be6621278cf298a84ab6f903adcb5
brian <ay83103@gmail.com>
2025-03-01 12:59:56 +0800

    feat: use dapper and postgreSQL

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace KanbanWebApi.Controllers
 {
@@ -6,28 +8,22 @@ namespace KanbanWebApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private readonly IDbConnection _connection;
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDbConnection connection)
         {
             _logger = logger;
+            _connection = connection;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet]
+        public async Task<object> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var sql = @"SELECT * FROM board;";
+
+            return await _connection.QueryAsync(sql);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using Npgsql;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// 讀取連線字串
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 註冊 IDbConnection，讓它每次請求時都會產生新的連線
+builder.Services.AddScoped<IDbConnection>(sp => new NpgsqlConnection(connectionString));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
